b4b936dadc10a6c868623f859fc946b9e10af2c2
Shalom <shalom.shawn@yahoo.com>
2020-06-08 17:52:50 +0100

    dffb

--- app.py
+++ app.py
@@ -11,9 +11,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
-app.secret_key = 'jose'
+app.secret_key = 'jos67890)(*&^)'
 api = Api(app)
-
 
 
 
